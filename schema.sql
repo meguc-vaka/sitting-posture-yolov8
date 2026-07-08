acac5a44f8d8d99429ef1ad7f3dd03fe9dd409c4
meguc-vaka <meguchandesyo@gmail.com>
2026-07-08 22:09:15 +0800

    Delete schema.sql

--- schema.sql
+++
@@ -1,9 +0,0 @@
-
-    DROP TABLE IF EXISTS users;
-    CREATE TABLE users (
-        userId INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        isAdmin INTEGER DEFAULT 0
-    );
-    
